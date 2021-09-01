180f81c13106acbe466afe0ac4ecce7344229c36
jdmejiav <jdmejiav@eafit.edu.co>
2021-08-31 19:46:37 -0500

    login

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 
 import "../styles/login.css";
 import NavBar from "../components/NavBar";
-import "firebase/auth";
-import { app } from "../firebaseConfig";
 
-import { Form, Icon, Input, Button } from "antd";
-import { async } from "q";
 
 export default class Login extends Component {
   constructor() {
@@ -18,42 +14,9 @@ export default class Login extends Component {
   }
 
   render() {
-    const handleOnSubmit = async (e) => {
-      await app
-        .auth()
-        .createUserWithEmailAndPassword(this.state.email, this.state.pass);
-      console.log("finalizó");
-    };
     return (
       <div>
         <NavBar />
-        <div className="login-container">
-          <div className="input-container">
-            <Form
-              action={(e) => {
-                console.log("se manda")
-                handleOnSubmit();
-                e.preventDefault();
-              }}
-            >
-              <Input
-                type="email"
-                placeholder="correo@example.com"
-                onChange={(e) => {
-                  this.setState({ email: e.target.value });
-                }}
-              ></Input>
-              <Input
-                type="password"
-                placeholder="contraseña"
-                onChange={(e) => {
-                  this.setState({ email: e.target.value });
-                }}
-              ></Input>
-              <Input type="submit" value="Ingresar"></Input>
-            </Form>
-          </div>
-        </div>
       </div>
     );
   }
