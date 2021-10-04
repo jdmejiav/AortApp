718504edb8fe23c99a41b0b8a40b81766a2a001b
jdmejiav <jdmejiav@eafit.edu.co>
2021-10-03 23:25:17 -0500

    teacher recomendations on controllers

--- src/pages/cardpairs.js
+++ src/pages/cardpairs.js
@@ -5,13 +5,15 @@ import CardPairs from "../components/cardPairs";
 export default class CardPage extends Component {
   render() {
     return (
-      <div         style={{
-        
-        background: "rgb(255,255,255)",
-        background:
-          "radial-gradient(circle, rgba(255,255,255,1) 46%, rgba(157,205,241,1) 100%)",
-      }}>
-        <NavBarLog/>
+      <div
+        style={{
+          height:"100%",
+          background: "rgb(255,255,255)",
+          background:
+            "radial-gradient(circle, rgba(255,255,255,1) 46%, rgba(157,205,241,1) 100%)",
+        }}
+      >
+        <NavBarLog />
         <CardPairs />
       </div>
     );
